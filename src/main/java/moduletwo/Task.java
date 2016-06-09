6c8f7e3d6d5195a8c4347dd789a8e4d614b9ccc8
SleepWalker <vityagorbenko@yandex.ru>
2016-06-10 02:06:31 +0300

    initial commit

---
+++ src/main/java/moduletwo/Task.java
@@ -0,0 +1,14 @@
+package moduletwo;
+
+/**
+ * Created by User on 10.06.2016.
+ */
+public interface Task{
+
+    // Метода запускает таск на выполнение
+    void execute();
+
+    // Возвращает результат выполнения
+    Object getResult();
+
+}
---
+++ src/main/java/moduletwo/Validator.java
@@ -0,0 +1,11 @@
+package moduletwo;
+
+/**
+ * Created by User on 10.06.2016.
+ */
+public interface Validator {
+
+    // Валидирует переданое значение
+    boolean isValid(Object result);
+
+}
