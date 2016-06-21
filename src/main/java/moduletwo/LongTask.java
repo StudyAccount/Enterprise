b4e65d8c030111634d05664b11eab5dc93544c39
SleepWalker <vityagorbenko@yandex.ru>
2016-06-21 04:06:32 +0300

    module two rewrite task and validator

---
+++ src/main/java/moduletwo/LongTask.java
@@ -0,0 +1,29 @@
+package moduletwo;
+
+/**
+ * Created by User on 18.06.2016.
+ */
+public class LongTask implements Task<Long> {
+
+    private Long input;
+    private Long output;
+    private final static Long SOME_EXTRA_LONG_NUMBER = 10L;
+
+    public LongTask(Long input, Long output) {
+        this.input = input;
+        this.output = output;
+    }
+
+
+    @Override
+    public void execute() {
+
+        output = input + SOME_EXTRA_LONG_NUMBER;
+    }
+
+    @Override
+    public Long getResult() {
+
+        return output;
+    }
+}
---
+++ src/main/java/moduletwo/NumberValidator.java
@@ -0,0 +1,16 @@
+package moduletwo;
+
+/**
+ * Created by User on 18.06.2016.
+ */
+public class NumberValidator implements Validator<Number> {
+
+
+
+    @Override
+    public boolean isValid(Number result) {
+
+        boolean val = (result.longValue() > 0);
+        return val;
+    }
+}
